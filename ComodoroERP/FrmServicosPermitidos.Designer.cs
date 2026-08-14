32e24d8d7673852e7e5f736805d7de682f5b7c97
pauloponcz <paulo.ric03@gmail.com>
2026-08-14 03:42:42 -0300

    feat: melhora visual da tela de servicos permitidos

--- ComodoroERP/FrmServicosPermitidos.Designer.cs
+++ ComodoroERP/FrmServicosPermitidos.Designer.cs
@@ -31,17 +31,25 @@
         {
             dgvServicosPermitidos = new DataGridView();
             txtFiltroServico = new TextBox();
-            label3 = new Label();
+            lblFiltroServico = new Label();
             cmbFiltroCategoria = new ComboBox();
-            label8 = new Label();
+            lblFiltroCategoria = new Label();
             btnLimparFiltros = new Button();
             btnFiltrar = new Button();
             btnImportarLista = new Button();
             btnFechar = new Button();
-
+            pnlTopo = new Panel();
+            lblSubtituloTela = new Label();
+            lblTituloTela = new Label();
+            pnlFiltros = new Panel();
+            lblFiltros = new Label();
+            pnlGrid = new Panel();
+            lblServicosPermitidos = new Label();
             ((System.ComponentModel.ISupportInitialize)dgvServicosPermitidos).BeginInit();
+            pnlTopo.SuspendLayout();
+            pnlFiltros.SuspendLayout();
+            pnlGrid.SuspendLayout();
             SuspendLayout();
-
             // 
             // dgvServicosPermitidos
             // 
@@ -49,129 +57,213 @@
             dgvServicosPermitidos.AllowUserToDeleteRows = false;
             dgvServicosPermitidos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dgvServicosPermitidos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvServicosPermitidos.Location = new Point(21, 70);
+            dgvServicosPermitidos.Location = new Point(15, 45);
             dgvServicosPermitidos.MultiSelect = false;
             dgvServicosPermitidos.Name = "dgvServicosPermitidos";
             dgvServicosPermitidos.ReadOnly = true;
             dgvServicosPermitidos.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgvServicosPermitidos.Size = new Size(755, 296);
+            dgvServicosPermitidos.Size = new Size(910, 235);
             dgvServicosPermitidos.TabIndex = 38;
-
             // 
             // txtFiltroServico
             // 
-            txtFiltroServico.Location = new Point(272, 12);
+            txtFiltroServico.Location = new Point(310, 65);
             txtFiltroServico.Name = "txtFiltroServico";
-            txtFiltroServico.Size = new Size(126, 23);
+            txtFiltroServico.Size = new Size(330, 25);
             txtFiltroServico.TabIndex = 37;
-
             // 
-            // label3
+            // lblFiltroServico
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(224, 15);
-            label3.Name = "label3";
-            label3.Size = new Size(48, 15);
-            label3.TabIndex = 36;
-            label3.Text = "Serviço:";
-
+            lblFiltroServico.AutoSize = true;
+            lblFiltroServico.Location = new Point(310, 42);
+            lblFiltroServico.Name = "lblFiltroServico";
+            lblFiltroServico.Size = new Size(55, 19);
+            lblFiltroServico.TabIndex = 36;
+            lblFiltroServico.Text = "Serviço:";
             // 
             // cmbFiltroCategoria
             // 
             cmbFiltroCategoria.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbFiltroCategoria.FormattingEnabled = true;
-            cmbFiltroCategoria.Location = new Point(77, 12);
+            cmbFiltroCategoria.Location = new Point(20, 65);
             cmbFiltroCategoria.Name = "cmbFiltroCategoria";
-            cmbFiltroCategoria.Size = new Size(136, 23);
+            cmbFiltroCategoria.Size = new Size(260, 25);
             cmbFiltroCategoria.TabIndex = 35;
-
             // 
-            // label8
+            // lblFiltroCategoria
             // 
-            label8.AutoSize = true;
-            label8.Location = new Point(16, 15);
-            label8.Name = "label8";
-            label8.Size = new Size(61, 15);
-            label8.TabIndex = 34;
-            label8.Text = "Categoria:";
-
+            lblFiltroCategoria.AutoSize = true;
+            lblFiltroCategoria.Location = new Point(20, 42);
+            lblFiltroCategoria.Name = "lblFiltroCategoria";
+            lblFiltroCategoria.Size = new Size(71, 19);
+            lblFiltroCategoria.TabIndex = 34;
+            lblFiltroCategoria.Text = "Categoria:";
             // 
             // btnLimparFiltros
             // 
-            btnLimparFiltros.Location = new Point(126, 41);
+            btnLimparFiltros.Location = new Point(815, 58);
             btnLimparFiltros.Name = "btnLimparFiltros";
-            btnLimparFiltros.Size = new Size(99, 23);
+            btnLimparFiltros.Size = new Size(95, 35);
             btnLimparFiltros.TabIndex = 40;
-            btnLimparFiltros.Text = "Limpar Filtros";
+            btnLimparFiltros.Text = "Limpar";
             btnLimparFiltros.UseVisualStyleBackColor = true;
             btnLimparFiltros.Click += btnLimparFiltros_Click;
-
             // 
             // btnFiltrar
             // 
-            btnFiltrar.Location = new Point(21, 41);
+            btnFiltrar.Location = new Point(705, 58);
             btnFiltrar.Name = "btnFiltrar";
-            btnFiltrar.Size = new Size(99, 23);
+            btnFiltrar.Size = new Size(95, 35);
             btnFiltrar.TabIndex = 39;
             btnFiltrar.Text = "Filtrar";
             btnFiltrar.UseVisualStyleBackColor = true;
             btnFiltrar.Click += btnFiltrar_Click;
-
             // 
             // btnImportarLista
             // 
-            btnImportarLista.Location = new Point(231, 41);
+            btnImportarLista.Location = new Point(20, 540);
             btnImportarLista.Name = "btnImportarLista";
-            btnImportarLista.Size = new Size(99, 23);
+            btnImportarLista.Size = new Size(130, 35);
             btnImportarLista.TabIndex = 41;
             btnImportarLista.Text = "Importar Lista";
             btnImportarLista.UseVisualStyleBackColor = true;
             btnImportarLista.Click += btnImportarLista_Click;
-
             // 
             // btnFechar
             // 
-            btnFechar.Location = new Point(21, 372);
+            btnFechar.Location = new Point(840, 540);
             btnFechar.Name = "btnFechar";
-            btnFechar.Size = new Size(99, 23);
+            btnFechar.Size = new Size(120, 35);
             btnFechar.TabIndex = 42;
             btnFechar.Text = "Fechar";
             btnFechar.UseVisualStyleBackColor = true;
             btnFechar.Click += btnFechar_Click;
-
+            // 
+            // pnlTopo
+            // 
+            pnlTopo.BackColor = Color.SteelBlue;
+            pnlTopo.Controls.Add(lblSubtituloTela);
+            pnlTopo.Controls.Add(lblTituloTela);
+            pnlTopo.Location = new Point(0, 0);
+            pnlTopo.Name = "pnlTopo";
+            pnlTopo.Size = new Size(1000, 75);
+            pnlTopo.TabIndex = 43;
+            // 
+            // lblSubtituloTela
+            // 
+            lblSubtituloTela.AutoSize = true;
+            lblSubtituloTela.Font = new Font("Segoe UI", 9F);
+            lblSubtituloTela.ForeColor = Color.WhiteSmoke;
+            lblSubtituloTela.Location = new Point(27, 43);
+            lblSubtituloTela.Name = "lblSubtituloTela";
+            lblSubtituloTela.Size = new Size(369, 15);
+            lblSubtituloTela.TabIndex = 1;
+            lblSubtituloTela.Text = "Consulte e importe a lista de serviços permitidos para os orçamentos";
+            // 
+            // lblTituloTela
+            // 
+            lblTituloTela.AutoSize = true;
+            lblTituloTela.Font = new Font("Segoe UI Semibold", 15F);
+            lblTituloTela.ForeColor = Color.White;
+            lblTituloTela.Location = new Point(25, 14);
+            lblTituloTela.Name = "lblTituloTela";
+            lblTituloTela.Size = new Size(223, 28);
+            lblTituloTela.TabIndex = 0;
+            lblTituloTela.Text = "SERVIÇOS PERMITIDOS";
+            // 
+            // pnlFiltros
+            // 
+            pnlFiltros.BackColor = Color.White;
+            pnlFiltros.BorderStyle = BorderStyle.FixedSingle;
+            pnlFiltros.Controls.Add(lblFiltros);
+            pnlFiltros.Controls.Add(lblFiltroCategoria);
+            pnlFiltros.Controls.Add(cmbFiltroCategoria);
+            pnlFiltros.Controls.Add(lblFiltroServico);
+            pnlFiltros.Controls.Add(btnLimparFiltros);
+            pnlFiltros.Controls.Add(txtFiltroServico);
+            pnlFiltros.Controls.Add(btnFiltrar);
+            pnlFiltros.Location = new Point(20, 90);
+            pnlFiltros.Name = "pnlFiltros";
+            pnlFiltros.Size = new Size(940, 115);
+            pnlFiltros.TabIndex = 44;
+            // 
+            // lblFiltros
+            // 
+            lblFiltros.AutoSize = true;
+            lblFiltros.Font = new Font("Segoe UI Semibold", 11F);
+            lblFiltros.ForeColor = Color.DimGray;
+            lblFiltros.Location = new Point(15, 12);
+            lblFiltros.Name = "lblFiltros";
+            lblFiltros.Size = new Size(51, 20);
+            lblFiltros.TabIndex = 0;
+            lblFiltros.Text = "Filtros";
+            // 
+            // pnlGrid
+            // 
+            pnlGrid.BackColor = Color.White;
+            pnlGrid.BorderStyle = BorderStyle.FixedSingle;
+            pnlGrid.Controls.Add(lblServicosPermitidos);
+            pnlGrid.Controls.Add(dgvServicosPermitidos);
+            pnlGrid.Location = new Point(20, 220);
+            pnlGrid.Name = "pnlGrid";
+            pnlGrid.Size = new Size(940, 300);
+            pnlGrid.TabIndex = 45;
+            // 
+            // lblServicosPermitidos
+            // 
+            lblServicosPermitidos.AutoSize = true;
+            lblServicosPermitidos.Font = new Font("Segoe UI Semibold", 11F);
+            lblServicosPermitidos.ForeColor = Color.DimGray;
+            lblServicosPermitidos.Location = new Point(15, 12);
+            lblServicosPermitidos.Name = "lblServicosPermitidos";
+            lblServicosPermitidos.Size = new Size(142, 20);
+            lblServicosPermitidos.TabIndex = 0;
+            lblServicosPermitidos.Text = "Serviços Permitidos";
             // 
             // FrmServicosPermitidos
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.WhiteSmoke;
+            ClientSize = new Size(984, 589);
+            Controls.Add(pnlGrid);
+            Controls.Add(pnlFiltros);
+            Controls.Add(pnlTopo);
             Controls.Add(btnFechar);
             Controls.Add(btnImportarLista);
-            Controls.Add(btnLimparFiltros);
-            Controls.Add(btnFiltrar);
-            Controls.Add(dgvServicosPermitidos);
-            Controls.Add(txtFiltroServico);
-            Controls.Add(label3);
-            Controls.Add(cmbFiltroCategoria);
-            Controls.Add(label8);
+            Font = new Font("Segoe UI", 10F);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
             Name = "FrmServicosPermitidos";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Serviços Permitidos";
-
             ((System.ComponentModel.ISupportInitialize)dgvServicosPermitidos).EndInit();
+            pnlTopo.ResumeLayout(false);
+            pnlTopo.PerformLayout();
+            pnlFiltros.ResumeLayout(false);
+            pnlFiltros.PerformLayout();
+            pnlGrid.ResumeLayout(false);
+            pnlGrid.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dgvServicosPermitidos;
         private TextBox txtFiltroServico;
-        private Label label3;
+        private Label lblFiltroServico;
         private ComboBox cmbFiltroCategoria;
-        private Label label8;
+        private Label lblFiltroCategoria;
         private Button btnLimparFiltros;
         private Button btnFiltrar;
         private Button btnImportarLista;
         private Button btnFechar;
+        private Panel pnlTopo;
+        private Label lblSubtituloTela;
+        private Label lblTituloTela;
+        private Panel pnlFiltros;
+        private Label lblFiltros;
+        private Panel pnlGrid;
+        private Label lblServicosPermitidos;
     }
 }
--- ComodoroERP/FrmServicosPermitidos.cs
+++ ComodoroERP/FrmServicosPermitidos.cs
@@ -14,7 +14,153 @@ namespace ComodoroERP
             ConfigurarGrid();
             CarregarCategorias();
             CarregarServicos();
+            AplicarEstiloVisual();
         }
+
+        private void AplicarEstiloVisual()
+        {
+            BackColor = Color.WhiteSmoke;
+            Font = new Font("Segoe UI", 10);
+
+            AplicarEstiloPainel(pnlFiltros);
+            AplicarEstiloPainel(pnlGrid);
+
+            AplicarEstiloBotaoPrincipal(btnFiltrar);
+            AplicarEstiloBotaoPrincipal(btnLimparFiltros);
+            AplicarEstiloBotaoPrincipal(btnImportarLista);
+            AplicarEstiloBotaoCancelar(btnFechar);
+
+            AplicarEstiloGrid(dgvServicosPermitidos);
+
+            AplicarEstiloCampos(this);
+
+            pnlTopo.BackColor = Color.SteelBlue;
+
+            lblTituloTela.ForeColor = Color.White;
+            lblTituloTela.Font = new Font("Segoe UI Semibold", 15);
+
+            lblSubtituloTela.ForeColor = Color.WhiteSmoke;
+            lblSubtituloTela.Font = new Font("Segoe UI", 9);
+
+            lblFiltros.ForeColor = Color.FromArgb(35, 35, 35);
+            lblFiltros.Font = new Font("Segoe UI Semibold", 11);
+
+            lblServicosPermitidos.ForeColor = Color.FromArgb(35, 35, 35);
+            lblServicosPermitidos.Font = new Font("Segoe UI Semibold", 11);
+        }
+
+        private void AplicarEstiloPainel(Panel painel)
+        {
+            painel.BackColor = Color.White;
+            painel.BorderStyle = BorderStyle.FixedSingle;
+        }
+
+        private void AplicarEstiloBotaoPrincipal(Button botao)
+        {
+            botao.FlatStyle = FlatStyle.Flat;
+            botao.BackColor = Color.White;
+            botao.ForeColor = Color.FromArgb(45, 45, 45);
+            botao.Font = new Font("Segoe UI Semibold", 10);
+            botao.Cursor = Cursors.Hand;
+
+            botao.FlatAppearance.BorderColor = Color.Gainsboro;
+            botao.FlatAppearance.BorderSize = 1;
+
+            botao.MouseEnter += (s, e) =>
+            {
+                botao.BackColor = Color.AliceBlue;
+            };
+
+            botao.MouseLeave += (s, e) =>
+            {
+                botao.BackColor = Color.White;
+            };
+        }
+
+        private void AplicarEstiloBotaoCancelar(Button botao)
+        {
+            botao.FlatStyle = FlatStyle.Flat;
+            botao.BackColor = Color.White;
+            botao.ForeColor = Color.DimGray;
+            botao.Font = new Font("Segoe UI Semibold", 10);
+            botao.Cursor = Cursors.Hand;
+
+            botao.FlatAppearance.BorderColor = Color.Silver;
+            botao.FlatAppearance.BorderSize = 1;
+
+            botao.MouseEnter += (s, e) =>
+            {
+                botao.BackColor = Color.Gainsboro;
+            };
+
+            botao.MouseLeave += (s, e) =>
+            {
+                botao.BackColor = Color.White;
+            };
+        }
+
+        private void AplicarEstiloGrid(DataGridView grid)
+        {
+            grid.BackgroundColor = Color.White;
+            grid.BorderStyle = BorderStyle.None;
+            grid.GridColor = Color.Gainsboro;
+
+            grid.EnableHeadersVisualStyles = false;
+
+            grid.ColumnHeadersDefaultCellStyle.BackColor = Color.SteelBlue;
+            grid.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            grid.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI Semibold", 9);
+            grid.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+
+            grid.DefaultCellStyle.BackColor = Color.White;
+            grid.DefaultCellStyle.ForeColor = Color.FromArgb(45, 45, 45);
+            grid.DefaultCellStyle.SelectionBackColor = Color.AliceBlue;
+            grid.DefaultCellStyle.SelectionForeColor = Color.Black;
+            grid.DefaultCellStyle.Font = new Font("Segoe UI", 9);
+
+            grid.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(248, 248, 248);
+
+            grid.RowHeadersVisible = false;
+            grid.AllowUserToAddRows = false;
+            grid.AllowUserToDeleteRows = false;
+            grid.ReadOnly = true;
+            grid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            grid.MultiSelect = false;
+            grid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void AplicarEstiloCampos(Control controlePai)
+        {
+            foreach (Control controle in controlePai.Controls)
+            {
+                if (controle is Label label)
+                {
+                    if (label.Parent == pnlTopo)
+                        continue;
+
+                    label.ForeColor = Color.DimGray;
+                    label.Font = new Font("Segoe UI", 9);
+                }
+
+                if (controle is TextBox textBox)
+                {
+                    textBox.BorderStyle = BorderStyle.FixedSingle;
+                    textBox.Font = new Font("Segoe UI", 10);
+                }
+
+                if (controle is ComboBox comboBox)
+                {
+                    comboBox.FlatStyle = FlatStyle.Flat;
+                    comboBox.Font = new Font("Segoe UI", 10);
+                }
+
+                if (controle.HasChildren)
+                {
+                    AplicarEstiloCampos(controle);
+                }
+            }
+        }
+
         private void dgvServicosPermitidos_DataError(object sender, DataGridViewDataErrorEventArgs e)
         {
             e.ThrowException = false;
@@ -34,7 +180,6 @@ namespace ComodoroERP
 
             dgvServicosPermitidos.DataError += dgvServicosPermitidos_DataError;
         }
-
 
         private void CarregarCategorias()
         {
@@ -124,36 +269,6 @@ namespace ComodoroERP
             catch (Exception ex)
             {
                 MessageBox.Show("Erro ao importar CSV: " + ex.Message);
-            }
-        }
-        private void ImportarListaPadrao()
-        {
-            try
-            {
-                // Por enquanto vamos inserir alguns exemplos.
-                // Depois trocamos por importação de CSV/Excel completa.
-
-                _service.InserirServicoSeNaoExistir("SERVIÇO DE MANUTENÇÃO ELÉTRICA", "INSTALAÇÃO DE LUMINÁRIA");
-                _service.InserirServicoSeNaoExistir("SERVIÇO DE MANUTENÇÃO ELÉTRICA", "INSTALAÇÃO DE REFLETOR");
-                _service.InserirServicoSeNaoExistir("SERVIÇO DE MANUTENÇÃO ELÉTRICA", "INSTALAÇÃO OU TROCA DE LÂMPADAS");
-                _service.InserirServicoSeNaoExistir("SERVIÇO DE MANUTENÇÃO ELÉTRICA", "REPARO DE LUMINÁRIA");
-
-                _service.InserirServicoSeNaoExistir("SERVIÇO DE MANUTENÇÃO HIDRÁULICA", "DESENTUPIMENTO DE VASO SANITÁRIO");
-                _service.InserirServicoSeNaoExistir("SERVIÇO DE MANUTENÇÃO HIDRÁULICA", "REPARO DE REDE HIDRÁULICA");
-                _service.InserirServicoSeNaoExistir("SERVIÇO DE MANUTENÇÃO HIDRÁULICA", "SUBSTITUIÇÃO DE ASSENTO SANITÁRIO");
-
-                _service.InserirServicoSeNaoExistir("SERVIÇO DE MANUTENÇÃO DE PINTURA, TELHADO E PISO", "REPARO DE TELHADO COM AUTORIZAÇÃO DA SME");
-                _service.InserirServicoSeNaoExistir("SERVIÇO DE MANUTENÇÃO DE PINTURA, TELHADO E PISO", "PINTURA INTERNA E EXTERNA MANTENDO PADRAO E COR");
-                _service.InserirServicoSeNaoExistir("SERVIÇO DE MANUTENÇÃO DE PINTURA, TELHADO E PISO", "REPARO DE PISO");
-
-                MessageBox.Show("Lista padrão importada com sucesso.");
-
-                CarregarCategorias();
-                CarregarServicos();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Erro ao importar lista: " + ex.Message);
             }
         }
 
